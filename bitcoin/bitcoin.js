0240599d2b8d0cf09aba37e25a17767c1c4a41eb
Varun Jain <varun@tessmen.com>
2013-10-16 20:32:43 -0700

    Added in MtGox/BTCAvg api sampels

--- bitcoin/bitcoin.js
+++ bitcoin/bitcoin.js
@@ -1,28 +1,45 @@
 if (Meteor.isClient) {
-  Template.btcavg.value = function () {
-    //var retVal = updateValue();
-    retVal = '';
-    return retVal;
+  Template.btcavg_usd.rendered = function () {
+      updateBTCAvgValue();
   };
 
-  Template.btcavg.events({
+  Template.btcavg_usd.events({
     'click input' : function () {
       // template data, if any, is available in 'this'
-      updateValue();
+      updateBTCAvgValue();
     }
   });
+
+  function updateBTCAvgValue() {
+    Meteor.http.get("https://api.bitcoinaverage.com/ticker/USD", function (err, res) {
+      var returnVal = res.data['last'];
+      //console.log(res.data);
+      console.log(returnVal);
+      $('#btcavg_usd_last').html(returnVal);
+    });
+  }
+
+  Template.mtgox_usd.rendered = function () {
+      updateMtGoxValue();
+  };
+
+  Template.mtgox_usd.events({
+    'click input' : function () {
+      // template data, if any, is available in 'this'
+      updateMtGoxValue();
+    }
+  });
+
+  function updateMtGoxValue() {
+    Meteor.http.get("http://data.mtgox.com/api/2/BTCUSD/money/ticker", function(err,res) {
+      $('#mtgox_usd_last').html(res.data['data']['last']['value']);
+    })
+  }
+
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
   });
-}
-
-function updateValue() {
-  Meteor.http.get("https://api.bitcoinaverage.com/ticker/USD", function (err, res) {
-    //var returnVal = JSON.parse(res.data);
-    console.log(res.data);
-  });
-  return 'vj';
 }
